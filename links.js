a2b030303fd294502c46747001bf6016e600500b
CGS-Jack-Bashford <jack.bashford42@gmail.com>
2018-08-10 13:22:30 +1000

    Added links.js

--- links.js
+++ links.js
@@ -15,4 +15,13 @@ function loadFile(name, type){
     }
 }
 
-loadFile("index.css", "css");
+//CSS
+loadFile("index.css", "css");                                                                //Main CSS File
+loadFile("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css", "css"); //Animate.css
+loadFile("https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css", "css");     //Bulma
+loadFile("https://use.fontawesome.com/releases/v5.2.0/css/all.css", "css");                  //Font Awesome
+
+//JS
+loadFile("https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js", "js");          //jQuery
+loadFile("https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js", "js");                   //Wow.js
+loadFile("https://cdnjs.cloudflare.com/ajax/libs/less.js/3.7.1/less.min.js", "js");          //LessCss.js
